5dd5c095efdb866f1b814a616bda8c939eb082c6
Juneki Hong <junekihong@gmail.com>
2016-04-25 20:53:11 -0400

    cleaned up the script to process everything else after the gro conversion

--- dataprep.sh
+++ dataprep.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-
-#for file; do
-#    ./midi2gro "$file" gro/
-#    echo
-#done
-
-
 for file; do
     bash test_pipeline.sh "$file"
 done
-
-
-
-
-#for file in gro/*; do
-#    echo "$file"
-#    ./gro2midi "$file" midi/
-#    read key
-#done
